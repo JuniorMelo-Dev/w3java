8a94c0f54256d8152484ad5b47023fe7937fe5e8
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-04-10 21:00:58 -0300

    Enum em um Switch Case

--- src/Main4.java
+++ src/Main4.java
@@ -6,7 +6,18 @@ public class Main4 {
     }
 
     public static void main(String[] args) {
-        Level myVar = Level.MEDIUM;
-        System.out.println(myVar);
+        Level myVar = Level.LOW;
+        
+        switch (myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
     }
 }
