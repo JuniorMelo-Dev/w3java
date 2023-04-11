18bf4fa60fcec410bb6dc35f00059722b6d65b8b
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-04-10 21:08:22 -0300

    Teste

--- src/Main4.java
+++ src/Main4.java
@@ -6,7 +6,7 @@ public class Main4 {
     }
 
     public static void main(String[] args) {
-        Level myVar = Level.LOW;
+        Level myVar = Level.HIGH;
         
         switch (myVar) {
             case LOW:
